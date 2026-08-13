ccfec98a4772ebdede06920d103e7eb820b3d907
zahidshaikmohammed-cmyk <zahidshaikmohammed@gmail.com>
2026-08-13 17:48:26 +0530

    Define behavioural feature contract

---
+++ research/features.py
@@ -0,0 +1,21 @@
+"""Canonical behavioural feature names.
+
+Implementation comes after the data schema and validation contract are locked.
+"""
+
+FEATURES = [
+    "intraday_range",
+    "range_efficiency",
+    "trend_persistence",
+    "chop_ratio",
+    "vwap_distance",
+    "opening_range_breakout_rate",
+    "breakout_continuation_rate",
+    "relative_volume",
+    "volume_price_efficiency",
+    "sector_relative_strength",
+    "benchmark_relative_strength",
+    "decoupling_score",
+    "max_favourable_excursion",
+    "max_adverse_excursion",
+]
